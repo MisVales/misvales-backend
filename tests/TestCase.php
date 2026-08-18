7c8fd41a67f71a14617ce6fcb17f289414003a18
thenotoriousxul <saulsanchezlopez999@gmail.com>
2026-08-18 15:57:59 -0600

    fix(testing): configure TestCase with DatabaseTransactions to preserve records in misvales

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use DatabaseTransactions;
+
+    /**
+     * Previene que RefreshDatabase ejecute migrate:fresh en la base de datos
+     */
+    public function refreshTestDatabase(): void
+    {
+        $this->beginDatabaseTransaction();
+    }
 }
